68edc142e8ee60771ae6eef2185656d252f2ee0d
ClaraCrazy <clara@cynthialabs.net>
2024-05-23 03:02:50 +0200

    Sluthub branding

--- packaging/docker/contents/docker-entrypoint.sh
+++ packaging/docker/contents/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 ## If the command has not been replaced by the user (i.e docker run image /bin/sh),
 ## follow through the setup process
 if [[ "$*" = "nginx -g daemon off;" ]]; then
-    echo "==== Starting Jellyfin Vue setup ===="
+    echo "==== Starting Sluthub setup ===="
     echo
     /setup.sh
     echo
